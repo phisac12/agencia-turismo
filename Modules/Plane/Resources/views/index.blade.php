4b1c3fc3b4a616d8718ee276e86ae2b9bf5ad56e
Isaac <isaac_contest@hotmail.com>
2022-01-24 12:01:04 -0300

    Adição e limpeza do modulo Plane

---
+++ Modules/Plane/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('plane::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('plane.name') !!}
+    </p>
+@endsection
