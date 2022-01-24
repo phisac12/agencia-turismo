686ca186d5551c907fd3477de7fc3a0dbb36d028
Isaac <isaac_contest@hotmail.com>
2022-01-24 12:10:16 -0300

    Refatoração nos modulos

--- Modules/State/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('state::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('state.name') !!}
-    </p>
-@endsection
